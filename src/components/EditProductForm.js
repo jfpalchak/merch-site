b95f4e7c17b2b86b240c72d05a4dd395f87a7363
Joey Palchak <joey.palchak@gmail.com>
2023-11-16 11:36:10 -0800

    create handleFormSubmission for EditProductForm

--- src/components/EditProductForm.js
+++ src/components/EditProductForm.js
@@ -1,18 +1,33 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
 
-function EditProductForm() {
+function EditProductForm(props) {
+
+  const { product } = props;
+
+  function handleFormSubmission(event) {
+    event.preventDefault();
+
+    props.onFormSubmission({
+      name: event.target.name.value,
+      description: event.target.description.value,
+      quantity: parseInt(event.target.quantity.value),
+      id: product.id
+    });
+  }
 
   return (
     <ReusableForm 
-      formSubmissionHandler={"placeholder"}
+      formSubmissionHandler={handleFormSubmission}
       buttonText="Update"
     />
   );
 }
 
-// EditProductForm.propTypes = {
-
-// };
+EditProductForm.propTypes = {
+  product: PropTypes.object,
+  onFormSubmission: PropTypes.func
+};
 
 export default EditProductForm;

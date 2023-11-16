b1fbc70965d0cfcc9a2fb9cd2f630042304a19fe
Joey Palchak <joey.palchak@gmail.com>
2023-11-16 09:12:04 -0800

    change test list data id's to string types

--- src/components/test-inventory.js
+++ src/components/test-inventory.js
@@ -3,18 +3,18 @@ export const testInventory = [
     name: "Headphones",
     description: "Black",
     quantity: 2,
-    id: 1
+    id: "1"
   },
   {
     name: "Speakers",
     description: "Walnut",
     quantity: 4,
-    id: 2
+    id: "2"
   },
   {
     name: "Record Player",
     description: "Bluetooth",
     quantity: 6,
-    id: 3
+    id: "3"
   }
 ];
